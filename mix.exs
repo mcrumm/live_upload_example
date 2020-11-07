87fd5f58e0deccfdc0253e5c44c8eb88ccee1434
Michael Crumm <mcrumm@gmail.com>
2020-11-06 16:48:38 -0800

    use lv latest

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Drops.MixProject do
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "~> 0.14.6", override: true, github: "phoenixframework/phoenix_live_view", branch: "master"},
+      {:phoenix_live_view, "~> 0.15.0-dev", override: true, github: "phoenixframework/phoenix_live_view", branch: "master"},
       {:floki, ">= 0.27.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
