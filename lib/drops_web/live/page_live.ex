0ff89c81b4438fea1f0bb1a11403fe03357912bf
Michael Crumm <michael.crumm@cargosense.com>
2021-02-01 11:50:09 -0800

    use code.priv_dir to build uploads path

--- lib/drops_web/live/page_live.ex
+++ lib/drops_web/live/page_live.ex
@@ -3,7 +3,15 @@ defmodule DropsWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    File.mkdir_p!("priv/static/uploads")
+    # Ensure the uploads path exists.
+    #
+    # Note: we are creating this directory on mount in order
+    # to simplify running the demo app. For your application,
+    # creating the directory on mount may not be preferable.
+    if connected?(socket) do
+      path = uploads_path()
+      File.mkdir_p!(path)
+    end
 
     {:ok,
      socket
@@ -25,11 +33,15 @@ defmodule DropsWeb.PageLive do
   def handle_event("save", _params, socket) do
     uploaded_files =
       consume_uploaded_entries(socket, :exhibit, fn %{path: path}, _entry ->
-        dest = Path.join("priv/static/uploads", Path.basename(path))
+        dest = Path.join(uploads_path(), Path.basename(path))
         File.cp!(path, dest)
         Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
       end)
 
     {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+  end
+
+  defp uploads_path do
+    Path.join([:code.priv_dir(:drops), "static", "uploads"])
   end
 end
