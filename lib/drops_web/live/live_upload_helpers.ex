6c5dad991f72396d8e4afe6027e428dbb373b7c5
Michael Crumm <michael.crumm@cargosense.com>
2021-04-17 02:02:04 -0700

    use lv latest and upload links to docs

--- lib/drops_web/live/live_upload_helpers.ex
+++ lib/drops_web/live/live_upload_helpers.ex
@@ -2,15 +2,6 @@ defmodule DropsWeb.LiveUploadHelpers do
   alias Phoenix.LiveView.UploadConfig
 
   @doc """
-  A work-around to return the "too many files" error.
-  """
-  def upload_error(%UploadConfig{} = config) do
-    for {ref, error} <- config.errors, ref == config.ref, reduce: nil do
-      _ -> error
-    end
-  end
-
-  @doc """
   Returns an error message for an upload error.
 
   See [`upload_errors/2`](https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.Helpers.html#upload_errors/2).
