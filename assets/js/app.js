e86b1ca9de5fd4d8c91f1785ea17397ab8cc29b6
Michael Crumm <michael.crumm@cargosense.com>
2021-04-20 14:24:30 -0700

    Replace NProgress by topbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@ import "../css/app.scss"
 //
 import "phoenix_html"
 import { Socket } from "phoenix"
-import NProgress from "nprogress"
+import topbar from "topbar"
 import { LiveSocket } from "phoenix_live_view"
 import Uploaders from "./uploaders"
 
@@ -22,8 +22,9 @@ let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("
 let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, uploaders: Uploaders })
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", info => NProgress.start())
-window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
+window.addEventListener("phx:page-loading-start", () => topbar.show())
+window.addEventListener("phx:page-loading-stop", () => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
