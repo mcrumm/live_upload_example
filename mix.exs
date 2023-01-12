8b6bb95526bc1fe8ff8afed3bb95b852045c9ce4
Michael Crumm <michael.crumm@cargosense.com>
2023-01-12 14:05:10 -0800

    Update phoenix_live_view to v0.18.6

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Drops.MixProject do
       {:ecto_sql, "~> 3.4"},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "0.18.4", phoenix_live_view_opts()},
+      {:phoenix_live_view, "~> 0.18.0", phoenix_live_view_opts()},
       {:floki, ">= 0.27.0", only: :test},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
