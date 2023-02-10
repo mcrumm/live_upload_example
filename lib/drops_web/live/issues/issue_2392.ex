e8ab9d0aa80d2c5c6795801bb45ecb45f82ed522
Michael Crumm <michael.crumm@cargosense.com>
2023-02-10 13:14:51 -0800

    Finish issue 2392

--- lib/drops_web/live/issues/issue_2392.ex
+++ lib/drops_web/live/issues/issue_2392.ex
@@ -37,26 +37,32 @@ defmodule DropsWeb.IssuesLive.Issue2392 do
         >
           <label for={@uploads.avatar.ref}>Drag and drop or select files:</label>
           <.live_file_input upload={@uploads.avatar} />
+          <input type="submit" value="Submit" />
         </form>
       </section>
 
       <section class="column">
-        <section
-          class="pending-uploads"
-          phx-drop-target={@uploads.avatar.ref}
-          style="min-height: 100%;"
-        >
+        <section class="pending-uploads">
           <h2>Preview</h2>
 
-          <%= for entry <- @uploads.avatar.entries do %>
-            <%= for error <- upload_errors(@uploads.avatar, entry) do %>
-              <p class="alert alert-danger"><%= upload_error_to_string(error) %></p>
-            <% end %>
+          <div :for={entry <- @uploads.avatar.entries}>
+            <p :for={error <- upload_errors(@uploads.avatar, entry)} class="alert alert-danger">
+              <%= upload_error_to_string(error) %>
+            </p>
             <.live_img_preview entry={entry} />
             <button type="button" class="button" phx-click="cancel-upload" phx-value-ref={entry.ref}>
               Cancel Upload
             </button>
-          <% end %>
+          </div>
+        </section>
+
+        <section class="uploaded-files">
+          <h2>Uploaded Files</h2>
+          <p>These files are temporary&mdash; the list will be refreshed after each upload.</p>
+          <figure :for={static_path <- @uploaded_files}>
+            <img src={static_path} />
+            <figcaption><%= static_path %></figcaption>
+          </figure>
         </section>
       </section>
     </section>
@@ -71,9 +77,12 @@ defmodule DropsWeb.IssuesLive.Issue2392 do
   @impl true
   def handle_event("save", _params, socket) do
     uploaded_files =
-      consume_uploaded_entries(socket, :avatar, fn params, _entry ->
-        IO.inspect(params, label: :upload_params)
-        {:ok, params}
+      consume_uploaded_entries(socket, :avatar, fn %{path: path}, _entry ->
+        # Drops.copy_entry_to_uploads(path)
+        dest = Path.join(Drops.uploads_priv_dir(), Path.basename(path))
+        File.cp!(path, dest)
+        static_path = Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
+        {:ok, static_path}
       end)
 
     {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
@@ -92,6 +101,6 @@ defmodule DropsWeb.IssuesLive.Issue2392 do
      |> allow_upload(:avatar,
        accept: ~w(image/jpeg image/png),
        max_entries: 1
-     )}
+     ), temporary_assigns: [uploaded_files: []]}
   end
 end
