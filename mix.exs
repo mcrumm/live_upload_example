82696440b509f45b1b4cb9312ffcd4efa14ae71e
Michael Crumm <michael.crumm@cargosense.com>
2023-07-07 10:08:09 -0700

    Update minor deps

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Drops.MixProject do
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:telemetry_metrics, "~> 0.6.1"},
-      {:telemetry_poller, "~> 0.4"},
+      {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
